ffa2068b7d927dd381faca7793f91a5c7299c0f1
vanessanaja <vanessacnaja@gmail.com>
2019-07-02 18:40:22 -0700

    create TodoApp Component

---
+++ src/TodoApp.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import Grid from '@material-ui/core/Grid';
+
+
+
+
+function TodoApp(){
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: '100vh',
+        backgroundColor: '#fafafa'
+  }}>
+
+    </Paper>
+  )
+}
+
+export default TodoApp;
+
+// - TodoApp
+//    -TodoForm
+//    -TodoList
+//      _TodoItem
