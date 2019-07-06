2d658e632f91cd70495093c4a9b565d37a0f217d
vanessanaja <vanessacnaja@gmail.com>
2019-07-06 12:37:47 -0700

    add checkbox for completed

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function Todo({ task, completed }){
   return (
     <ListItem>
     <ListItemText>
+    <Checkbox tabIndex={-1} checked={completed}></Checkbox>
       {task}
     </ListItemText>
   </ListItem>
