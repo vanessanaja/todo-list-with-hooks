cdb861483dee73c19099fec381563d6f2294839f
vanessanaja <vanessacnaja@gmail.com>
2019-07-09 15:53:29 -0700

    add icons

--- src/Todo.js
+++ src/Todo.js
@@ -2,14 +2,26 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 function Todo({ task, completed }){
   return (
     <ListItem>
-    <ListItemText>
     <Checkbox tabIndex={-1} checked={completed}></Checkbox>
+    <ListItemText style={{textDecoration: completed ? 'line-through' : 'none'}}>
       {task}
     </ListItemText>
+    <ListItemSecondaryAction>
+      <IconButton aria-label="Delete">
+       <DeleteIcon />
+      </IconButton>
+      <IconButton aria-label="Edit">
+       <EditIcon />
+      </IconButton>
+    </ListItemSecondaryAction>
   </ListItem>
   );
 }
