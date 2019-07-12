ebd1bce2e7f761cb6de0b645e37f5804adeea4e1
vanessanaja <vanessacnaja@gmail.com>
2019-07-11 17:43:27 -0700

    EditTodoForm component

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import useToggleState from './Hooks/useToggleState';
+import useToggle from './Hooks/useToggleState';
+import EditTodoForm from './EditTodoForm';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -7,9 +10,13 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
+
 function Todo({ id, task, completed, removeTodo, toggleTodo }){
+  const [isEditing, toggle] = useToggleState(false);
   return (
     <ListItem>
+    {isEditing ? <EditTodoForm /> : 
+      <>
     <Checkbox tabIndex={-1} checked={completed} onClick={() => toggleTodo(id)} />
     <ListItemText 
       style={{textDecoration: completed ? 'line-through' : 'none'}}
@@ -20,10 +27,12 @@ function Todo({ id, task, completed, removeTodo, toggleTodo }){
       <IconButton aria-label="Delete" onClick={() => removeTodo(id)}>
        <DeleteIcon />
       </IconButton>
-      <IconButton aria-label="Edit">
+      <IconButton onClick={toggle} aria-label="Edit">
        <EditIcon />
       </IconButton>
     </ListItemSecondaryAction>
+    </> 
+    }
   </ListItem>
   );
 }
