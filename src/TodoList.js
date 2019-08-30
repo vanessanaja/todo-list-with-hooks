41bd5adef2c679d555e59d52d798735a1ddbe2fa
vanessanaja <vanessacnaja@gmail.com>
2019-08-30 15:32:33 -0700

    todoContext and TodosProvider

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,29 +2,27 @@ import React from 'react';
 import Todo from './Todo';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import uuid from "uuid/v4";
+import Divider from "@material-ui/core/Divider";
 
 function TodoList({ todos, removeTodo, toggleTodo, editTodo }){
   if (todos.length)
-  return(
+  return (
     <Paper>
       <List>
         {todos.map((todo, i) => (
-          <>
+          // To add a key to a fragment, we have to use the long-hand version
+          // rather than <> </>, we have to use <React.Fragment>
+          <React.Fragment key={i}>
             <Todo
             //can just pass in destructured todo instead of id, taks and completed
               {...todo}
-              // id={todo.id}
-              // task={todo.task}
               key={todo.id}
-              // completed={todo.completed}
               removeTodo={removeTodo}
               toggleTodo={toggleTodo}
               editTodo={editTodo}
             />
               {i < todos.length - 1 && <Divider />}
-          </>
+          </React.Fragment>
         ))}
       </List>
     </Paper>
---
+++ src/context/todosContext.js
@@ -0,0 +1,18 @@
+//context that will keep track of todos and all methods that interact with todos
+import React, { createContext } from 'react';
+import useTodoState from '../hooks/useTodoState';
+
+const defaultTodos = [
+  { id: 1, task: 'Grocery shop', completed: false },
+  { id: 2, task: 'Walk dog', completed: false }
+]
+export const TodosContext = createContext();
+
+export function TodosProvider(props){
+  const todoStuff = useTodoState(defaultTodos);
+return (
+    <TodosContext.Provider value={{todoStuff}}>
+      {props.children}
+    </TodosContext.Provider>
+  )
+}  
