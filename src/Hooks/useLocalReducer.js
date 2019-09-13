91f82bf41471252d37dd8d8f2889d94231ea8cf0
vanessanaja <vanessacnaja@gmail.com>
2019-09-13 14:30:10 -0700

    create useLocalReducer

---
+++ src/Hooks/useLocalReducer.js
@@ -0,0 +1,22 @@
+import { useReducer, useEffect } from 'react';
+
+function useLocalStorageReducer(key, defaultValue, reducer) {
+  //make piece of state based off of value in local localStorage or default
+  const [state, setState] = useReducer(reducer, defaultValue, () => {
+    let val;
+    try {
+       val = JSON.parse(window.localStorage.getItem(key) || String(defaultValue))
+    }
+    catch(e) {
+      val = defaultValue;
+    }
+    return val;
+  });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [state]);
+    
+  return[state, dispatch];
+}
+
+export { useLocalStorageReducer };
